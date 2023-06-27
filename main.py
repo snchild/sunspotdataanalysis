e7ddfd32396c155989dab42e9abd6b7ca09f4606
snchild <sierrachild@yahoo.com>
2023-06-27 11:45:57 -0600

    included comments for Q 1

--- main.py
+++ main.py
@@ -15,3 +15,12 @@ numberOfSunspots = rawData["Number of Sunspots"]
 
 py.plot(years,numberOfSunspots)
 py.show()
+
+#locate the maxima, calculate the time between each of the maxima, average them and get uncertainty
+#local maxima will be where a value is greater than the values surrounding it
+#will need to figure out how to include only the maxima that i mean and not the noise - might have the value be greater than the 10 data points surrounding it
+diff = np.diff(numberOfSunspots) #this doesn't fix it
+localMaximaIndices = np.where((diff[:-1] > 0) & (diff[1:] <= 0)) [0] + 1
+
+#answer question: what's the cycle of the the maxima itself?
+# do fast fourier transform
