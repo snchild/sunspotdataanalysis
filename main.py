e302ddf5e03ec26c136f39e51ef5eda449292ecc
snchild <sierrachild@yahoo.com>
2023-06-30 10:41:24 -0600

    add box and whisker plot and clean code

--- main.py
+++ main.py
@@ -17,13 +17,17 @@ numberOfSunspots = rawData["Number of Sunspots"]
 #locate the maxima, calculate the time between each of the maxima, average them and get uncertainty
 
 #split up data, find max of those sections
-sectionYearBoundaries = [1818.001,1823,1833,1843,1855,1867,1878.5,1889,1901,1912,1923,1933,1944,1954,1964.5,1975.5,1986,1997,2009,2019.832]
+sectionYearBoundaries = [1818.001,1823,1833,1843,1855,1867,1878.5,1889,1901,\
+                         1912,1923,1933,1944,1954,1964.5,1975.5,1986,1997,\
+                             2009,2019.832] #values found visually
 sectionBoundariesIndices=[]
+
 j=0
 for i in range(len(years)):#get a list of the indices corresponding to the boundaries
     if(abs(years[i] - sectionYearBoundaries[j]) <= 0.001):
         sectionBoundariesIndices.append(i)
         j+=1
+        
 #get a list of the max values in each of those sections
 maxYears=[]
 maxSunspots=[]
@@ -39,6 +43,7 @@ for k in range(1,len(sectionBoundariesIndices)):
     maxYears.append(years[indexOfMaxNum])
     maxSunspots.append(numberOfSunspots[indexOfMaxNum])
 
+#make a plot to visualize the maxima
 plt.figure(1)
 plt.plot(years,numberOfSunspots,'b',alpha=.5, label="Raw Data")
 plt.plot(maxYears,maxSunspots,'r', label="Peaks")
@@ -48,41 +53,53 @@ plt.ylabel('Number of sun spots')
 plt.legend()
 plt.show()
 
-avgCycle = np.mean(np.diff(maxYears))
-uncertCycle = np.std(np.diff(maxYears)) / np.sqrt(len(np.diff(maxYears)))
+cycleData = np.diff(maxYears)
+avgCycle = np.mean(cycleData)
+uncertCycle = np.std(cycleData) / np.sqrt(len(cycleData))
 
-print("The average cycle of sunspots are", avgCycle ,"+-",uncertCycle,"years long")
+print("The average cycle of sunspots are {:.3f} +- {:.3f} years long.".format(avgCycle, uncertCycle))
 
 ##############################################################################################
 #Question: According to this data, what is the predicted activity in this month, June 2023?
-#note that we are assuming that the end of this data is a local minimum, and we are also assuming that a minimum to a peak is exactly half of a cycle.
-#date that we are forecasting for: June 30, 2023
-#in years, this is 2023.495
+#note that we are assuming that the end of this data is a local minimum, 
+#and we are also assuming that a minimum to a peak is exactly half of a cycle.
+#date that we are forecasting for: June 30, 2023 = 2023.495 years
 
 #determine years between june 30 and end of data set
 June30InYears = 2023.495
 endOfData = years.iloc[-1]
-diffInYears = round(June30InYears - endOfData, 3) #makes sure the difference is rounded to three decimal places
-#determine how far that is from the peak of the cycle
-timeUntilPeak = round((avgCycle/2) - diffInYears, 3)
+diffInYears = June30InYears - endOfData #time between a minimum and June 30th
+timeUntilPeak = (avgCycle/2) - diffInYears #time between June 30th and the porjected maximum
 
-
-#get the values of the trend at that time
+#get the number of sunspots at the same time of the cycle as June 30th
+#need to round to three decimal places for the comparison with the years
 yearsFromPast = np.around(np.array(maxYears[1:]) - timeUntilPeak, 3)
 
-indicesFromPast = []
 sunspotsFromPast = []
 for value in yearsFromPast:
     if value in years.values:
         something = years[years == value]
-        indicesFromPast.append(something.index[0])
-        sunspotsFromPast.append(numberOfSunspots[something.index[0]])
+        #exclude the negative values, valid numbers are always positive
+        if (numberOfSunspots[something.index[0]] > -1): sunspotsFromPast.append(numberOfSunspots[something.index[0]]) 
+        
     
 #get min, max, mean
-june30Min = pd.Series(sunspotsFromPast).min()
-june30Max = pd.Series(sunspotsFromPast).max()
-june30Mean = pd.Series(sunspotsFromPast).mean()
-june30MeanUncert = np.std(pd.Series(sunspotsFromPast)) / np.sqrt(len(pd.Series(sunspotsFromPast)))
+pastSunspotsSeries = pd.Series(sunspotsFromPast) #turning the list into a series is easier for calculations
 
-print("The mean number of sunspots on a day like June 30, 2023 is", june30Mean, "+-", june30MeanUncert, ".")
-print("The range of numbers of sunspots are", june30Min, "-", june30Max, ". ")
+june30Min = pastSunspotsSeries.min()
+june30Max = pastSunspotsSeries.max()
+june30Mean = pastSunspotsSeries.mean()
+june30MeanUncert = np.std(pastSunspotsSeries) / np.sqrt(len(pastSunspotsSeries))
+
+print("The mean number of sunspots on a day like June 30, 2023 is {:.3f} +- {:.3f}.".capitalize().format(june30Mean,june30MeanUncert))
+print("The range of numbers of sunspots are {} - {}".format(june30Min, june30Max))
+
+#make a box and whisker plot of the number of sunspots
+data = {' ':sunspotsFromPast}
+df= pd.DataFrame(data)
+plt.figure(2)
+df.boxplot()
+plt.title("Number of Sunspots on days similar to June 30, 2023")
+plt.xlabel("Days like June 30,2023")
+plt.ylabel("Number of Sunspots")
+plt.show()
